5ffa508d0f07a461215a7f31736e57afec249221
zanra2401 <grubgrex@gmail.com>
2026-07-18 00:00:47 +0700

    fix bug no display

--- src/chip8/components/chip8.hpp
+++ src/chip8/components/chip8.hpp
@@ -13,13 +13,13 @@ class Chip8TE {
   private:
     uint16_t pc = 0x200;
     uint16_t current_opcode = 0;
-    Graphic graphic;
     Memory memory;
     Register register_c;
     PLoader program_loader;
     Opcode opcode_decoder;
     uint8_t *rom_bytes;
   public:
+    Graphic graphic;
     Chip8TE();
     void load_program(const char *file_name);
     void emulate_cycle();
